286475747be97561fa4d927367aee5f9ea76a49a
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-10-18 20:19:57 +0200

    Got concurrent tests to compile

--- intray-cli/test/Intray/Cli/ConcurrentSpec.hs
+++ intray-cli/test/Intray/Cli/ConcurrentSpec.hs
@@ -5,63 +5,48 @@ module Intray.Cli.ConcurrentSpec
   )
 where
 
-import Control.Concurrent.Async
 import Control.Monad.Logger
 import Control.Monad.Reader
 import qualified Data.Text as T
 import Intray.Cli.Commands.Add
+import Intray.Cli.Env
 import Intray.Cli.OptParse
+import Intray.Cli.Sqlite
 import Intray.Cli.Store
 import Intray.Cli.TestUtils
 import Intray.Server.TestUtils
 import TestImport
+import UnliftIO
 
 spec :: Spec
-spec = sequential $
-  withIntrayServer $
-    it "Going through the usual manual steps just works, even if multiple clients do it at the same time in the same place" $ \cenv ->
-      withSystemTempDir "intray-cli-test-cache" $ \cacheDir ->
-        withSystemTempDir "intray-cli-test-data" $ \dataDir -> do
-          let burl = baseUrl cenv
-          let envVars =
-                [ ("INTRAY_USERNAME", "testuser"),
-                  ("INTRAY_PASSWORD", "testpassword"),
-                  ("INTRAY_URL", showBaseUrl burl),
-                  ("INTRAY_CACHE_DIR", fromAbsDir cacheDir),
-                  ("INTRAY_DATA_DIR", fromAbsDir dataDir)
-                ]
+spec = sequential . cliMSpec $
+  it "Going through the usual manual steps just works, even if multiple clients do it at the same time in the same place" $ \env -> testCliM env $ do
+    dataDir <- asks envDataDir
+    cacheDir <- asks envCacheDir
+    mClientEnv <- asks envClientEnv
+    let envVars =
+          concat
+            [ [ ("INTRAY_USERNAME", "testuser"),
+                ("INTRAY_PASSWORD", "testpassword"),
+                ("INTRAY_CACHE_DIR", fromAbsDir cacheDir),
+                ("INTRAY_DATA_DIR", fromAbsDir dataDir)
+              ],
+              [ ("INTRAY_URL", showBaseUrl (baseUrl clientEnv))
+                | clientEnv <- maybeToList mClientEnv
+              ]
+            ]
 
-          let intray = intrayWithEnv envVars
-          intray ["register"]
-          intray ["login"]
-          let additions = 6
-          dontLog <- runNoLoggingT askLoggerIO
-          forConcurrently_ [1 .. additions] $ \i ->
-            -- We cannot use 'intray' here because that uses 'withArgs' which is not threadsafe and causes segfaults
-            do
-              let sets =
-                    Settings
-                      { setBaseUrl = Just burl,
-                        setCacheDir = cacheDir,
-                        setDataDir = dataDir,
-                        setSyncStrategy = AlwaysSync,
-                        setAutoOpen = DontAutoOpen
-                      }
-              flip runLoggingT dontLog $
-                flip runReaderT sets $
-                  addItem
-                    AddSettings
-                      { addSetContents = ["hello", "world", T.pack (show i)],
-                        addSetReadStdin = False,
-                        addSetRemote = False
-                      }
-          let sets =
-                Settings
-                  { setBaseUrl = Just burl,
-                    setCacheDir = cacheDir,
-                    setDataDir = dataDir,
-                    setSyncStrategy = NeverSync,
-                    setAutoOpen = DontAutoOpen
-                  }
-          s <- flip runLoggingT dontLog $ runReaderT readClientStoreSize sets
-          s `shouldBe` additions
+    let intray = liftIO . intrayWithEnv envVars
+    intray ["register"]
+    intray ["login"]
+    let additions = 6
+    forConcurrently_ [1 .. additions] $ \i -> do
+      -- We cannot use 'intray' here because that uses 'withArgs' which is not threadsafe and causes segfaults
+      addItem
+        AddSettings
+          { addSetContents = ["hello", "world", T.pack (show i)],
+            addSetReadStdin = False,
+            addSetRemote = False
+          }
+    s <- getStoreSize
+    liftIO $ s `shouldBe` additions
--- intray-cli/test/Intray/Cli/TestUtils.hs
+++ intray-cli/test/Intray/Cli/TestUtils.hs
@@ -1,6 +1,19 @@
-module Intray.Cli.TestUtils (intrayWithEnv) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Intray.Cli.TestUtils where
 
 import Intray.Cli (intrayCli)
+import Intray.Cli.DB
+import Intray.Cli.Env
+import Intray.Cli.OptParse.Types (AutoOpen (..), SyncStrategy (..))
+import Intray.Server.TestUtils
+import qualified Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Types as HTTP
+import Test.Syd
+import Test.Syd.Path
+import Test.Syd.Persistent.Sqlite
+import Test.Syd.Wai
 import TestImport
 
 intrayWithEnv :: [(String, String)] -> [String] -> IO ()
@@ -8,3 +21,42 @@ intrayWithEnv envVars args = do
   mapM_ (uncurry setEnv) envVars
   putStrLn $ unwords $ "RUNNING:" : "intray" : args
   withArgs args intrayCli
+
+type CliSpec = TestDef '[HTTP.Manager] Env
+
+testCliM :: Env -> CliM a -> IO a
+testCliM env func = do
+  dontLog <- runNoLoggingT askLoggerIO
+  runLoggingT (runReaderT func env) dontLog
+
+cliMSpec :: CliSpec -> Spec
+cliMSpec s = managerSpec $ do
+  describe "online, with autosync" $ setupAroundWith' (\man _ -> onlineWithAutosyncEnvSetupFunc man) s
+  describe "online, without autosync" $ setupAroundWith' (\man _ -> onlineWithoutAutosyncEnvSetupFunc man) s
+  describe "offline" $ setupAround offlineEnvSetupFunc s
+
+offlineEnvSetupFunc :: SetupFunc Env
+offlineEnvSetupFunc = do
+  let envBaseUrl = Nothing
+  envCacheDir <- tempDirSetupFunc "intray-cli-test-cache-dir"
+  envDataDir <- tempDirSetupFunc "intray-cli-test-data-dir"
+  envConnectionPool <- connectionPoolSetupFunc clientAutoMigration
+  let envSyncStrategy = NeverSync
+  let envAutoOpen = DontAutoOpen
+  let envClientEnv = Nothing
+  pure Env {..}
+
+onlineWithoutAutosyncEnvSetupFunc :: HTTP.Manager -> SetupFunc Env
+onlineWithoutAutosyncEnvSetupFunc man = do
+  cenv <- intrayTestClientEnvSetupFunc Nothing man
+  env <- offlineEnvSetupFunc
+  pure $
+    env
+      { envSyncStrategy = NeverSync,
+        envClientEnv = Just cenv
+      }
+
+onlineWithAutosyncEnvSetupFunc :: HTTP.Manager -> SetupFunc Env
+onlineWithAutosyncEnvSetupFunc man = do
+  env <- onlineWithoutAutosyncEnvSetupFunc man
+  pure $ env {envSyncStrategy = AlwaysSync}
